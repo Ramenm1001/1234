92502f6a3b6b6ff6cc5b0291142477db28fb357e
makar <tasher_nub@icloud.com>
2022-04-21 20:05:15 +1000

    1

--- strategia2.py
+++ strategia2.py
@@ -24,10 +24,10 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.move()
-        pygame.draw.rect(win, (255, 0, 0), self.rect)
+        pygame.draw.rect(win, self.color, self.rect)
 
 
-class A(pygame.sprite.Sprite):
+class Entites(pygame.sprite.Sprite):
     def __init__(self, x, y, group):
         super().__init__()
         self.rect = pygame.Rect(x, y, 50, 50)
@@ -38,12 +38,29 @@ class A(pygame.sprite.Sprite):
         pygame.draw.rect(win, self.color, self.rect)
 
 
+class Heal(pygame.sprite.Sprite):
+    def __init__(self, x, y, group):
+        super().__init__()
+        self.rect = pygame.Rect(x, y, 50, 50)
+        self.color = [0, 255, 255]
+        group.add(self)
+
+    def update(self):
+        pygame.draw.rect(win, self.color, self.rect)
+
+
 player = Player(500, 500)
 bots = pygame.sprite.Group()
+heals = pygame.sprite.Group()
 
-A(100, 150, bots)
-A(200, 150, bots)
-A(300, 250, bots)
+Entites(100, 150, bots)
+Entites(200, 150, bots)
+Entites(300, 250, bots)
+
+Heal(random.randint(50, 950), random.randint(50, 550), heals)
+Heal(random.randint(50, 950), random.randint(50, 550), heals)
+Heal(random.randint(50, 950), random.randint(50, 550), heals)
+
 
 run = True
 while run:
@@ -54,13 +71,22 @@ while run:
     win.fill((0,0,0))
     player.update()
     bots.update()
+    heals.update()
     if pygame.sprite.spritecollideany(player, bots):
-        pygame.sprite.spritecollideany(player, bots).color[1] -= 1
-        pygame.sprite.spritecollideany(player, bots).color[0] += 1
+        pygame.sprite.spritecollideany(player, bots).color[1] -= 3
+        pygame.sprite.spritecollideany(player, bots).color[0] += 3
         player.color[0] -= 1
         player.color[1] += 1
+        if player.color[0] == 0: pygame.quit()
         if pygame.sprite.spritecollideany(player, bots).color[1] == 0:
             pygame.sprite.spritecollideany(player, bots).kill()
-            A(300, 250, bots)
+            Entites(random.randint(50, 950), random.randint(50, 550), bots)
+    if pygame.sprite.spritecollideany(player, heals):
+        pygame.sprite.spritecollideany(player, heals).kill()
+        if player.color[0] >= 175: player.color[0] = 255
+        else: player.color[0] += 75
+        if player.color[1] <= 75: player.color[1] = 0
+        else: player.color[1] -= 75
+        Heal(random.randint(50, 950), random.randint(50, 550), heals)
     pygame.display.update()
 pygame.quit()
